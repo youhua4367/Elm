8a337ac506303206d795d5dfea17d51ab8ed37f1
youhua <lianzhongai7@gmail.com>
2025-12-16 22:05:13 +0800

    Elm初始化项目

---
+++ src/main/java/com/example/elm_m/ElmMApplication.java
@@ -0,0 +1,13 @@
+package com.example.elm_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elm_m/ElmMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elm_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
